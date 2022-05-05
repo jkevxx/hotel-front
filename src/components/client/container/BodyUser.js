c3552cdf6548937fb2a5cf6a0e4c44f6cd880be1
kevinpaezg <kevin.paez1010@gmail.com>
2022-05-05 15:59:12 -0500

    Check in and out and some styles

--- src/components/client/container/BodyUser.js
+++ src/components/client/container/BodyUser.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
+// Styles
 import './BodyUserStyle.css'
-import { Box, Grid, FormControl, InputLabel, Select, MenuItem, TextField } from '@mui/material'
-import CardUser from './CardUser'
 
+// Components
+import { Box, Grid, FormControl, InputLabel, Select, MenuItem, TextField, IconButton } from '@mui/material'
+// Components Date
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+
+// Icons
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+// Components
+import CardUser from './CardUser'
 
 const images = {
   room1: 'https://www.hotellastrojes.com.mx/images/galerias/habitacion-sencilla-1533165647.jpg',
@@ -23,111 +31,138 @@ function BodyUser() {
 
   const [room, setRom] = useState(images)
   const [typeRoom, setTypeRoom] = useState(typeRooms)
-  const [value, setValue] = useState(null);
+  const [checkin, setCheckin] = useState(null)
+  const [checkout, setCheckout] = useState(null)
 
   return (
-    <main className='container'>
-      <section className='container__img'>
-        <div className="content">
-          <div className="content__option">
-            <h2 className="content__option-title">
-              Enjoy your Dream Vacation
-            </h2>
-            <p className='content__option-subtitle'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quae pariatur nostrum doloribus itaque sint inventore voluptatibus
-            </p>
+    <>
+      <main className='container'>
+        <section className='container__img'>
+          <div className="content">
+            <div className="content__option">
+              <h2 className="content__option-title">
+                Enjoy your Dream Vacation
+              </h2>
+              <p className='content__option-subtitle'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quae pariatur nostrum doloribus itaque sint inventore voluptatibus
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <Box component='section'
-        sx={{
-          // width: '90vw',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
-          height: '200px',
-          // textAlign: 'center'
-        }}
-      >
-        <Box component='div'
+        <Box component='section'
           sx={{
-            width: '70vw',
-            padding: '20px',
-            // border: '1px solid',
-            boxShadow: '1px 2px 5px 0px rgba(0,0,0,0.75)'
+            // width: '90vw',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            height: '200px',
+            // textAlign: 'center'
           }}
         >
-          <Grid container>
-            <Grid item xs={3}>
-              <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                <InputLabel id="demo-select-small">Type</InputLabel>
-                <Select
-                  labelId="demo-select-small"
-                  id="demo-select-small"
-                  // value={age}
-                  label="Age"
-                  defaultValue=""
-                // onChange={handleChange}
-                >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  {typeRoom.map((type, index) => (
-                    <MenuItem value={type.name} key={index}>{type.name}</MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-
-            <Grid item xs={3}>
-              <FormControl sx={{ m: 1, minWidth: 120 }} size="small" >
-                <InputLabel id="demo-select-small">People</InputLabel>
-                <Select
-                  labelId="demo-select-small"
-                  id="demo-select-small"
-                  // value={age}
-                  label="Age"
-                  defaultValue=""
-                // onChange={handleChange}
-                >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={1}>1</MenuItem>
-                  <MenuItem value={2}>2</MenuItem>
-                  <MenuItem value={3}>3</MenuItem>
-                  <MenuItem value={4}>4</MenuItem>
-                  <MenuItem value={5}>5</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <Grid item xs={4}>
-                <DatePicker
-                  id="birthday"
-                  name="birthday"
-                  label="Birthday"
-                  value={value}
-                  onChange={(newValue) => {
-                    setValue(newValue);
-                  }}
-                  renderInput={(params) => (
-                    <TextField
-                      fullWidth
-                      {...params}
-                    // {...register("birthday")}
-                    // error={!!errors.birthday}
-                    // helperText={errors.birthday ? errors.birthday.message : ""}
-                    />
-                  )}
-                />
+          <Box component='div'
+            sx={{
+              width: '80vw',
+              padding: '20px',
+              boxShadow: '1px 2px 5px 0px rgba(0,0,0,0.75)',
+              borderRadius: '10px'
+            }}
+          >
+            <Grid container>
+              <Grid item xs={2} sx={{ marginLeft: '10px' }}>
+                <FormControl sx={{ m: 1, minWidth: 140 }} size="small">
+                  <InputLabel id="demo-select-small">Type</InputLabel>
+                  <Select
+                    labelId="demo-select-small"
+                    id="demo-select-small"
+                    // value={age}
+                    label="Age"
+                    defaultValue=""
+                  // onChange={handleChange}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    {typeRoom.map((type, index) => (
+                      <MenuItem value={type.name} key={index}>{type.name}</MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
               </Grid>
-            </LocalizationProvider>
 
-            {/* <Grid item xs={2}>
+              <Grid item xs={2}>
+                <FormControl sx={{ m: 1, minWidth: 140 }} size="small" >
+                  <InputLabel id="demo-select-small">People</InputLabel>
+                  <Select
+                    labelId="demo-select-small"
+                    id="demo-select-small"
+                    // value={age}
+                    label="Age"
+                    defaultValue=""
+                  // onChange={handleChange}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <Grid item xs={3} sx={{ margin: '0 30px 0 20px ' }}>
+                  <DatePicker
+                    id="chekin"
+                    name="chekin"
+                    label="Check-in"
+                    value={checkin}
+                    minDate={new Date()}
+                    // shouldDisableDate={(date) => date.getTime() === new Date('2022-05-03T00:00').getTime()}
+                    onChange={(newValue) => {
+                      setCheckin(newValue);
+                    }}
+                    renderInput={(params) => (
+                      <TextField
+                        // fullWidth
+                        {...params}
+                      // {...register("birthday")}
+                      // error={!!errors.birthday}
+                      // helperText={errors.birthday ? errors.birthday.message : ""}
+                      />
+                    )}
+                  />
+                </Grid>
+              </LocalizationProvider>
+
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <Grid item xs={3} >
+                  <DatePicker
+                    id="chekout"
+                    name="chekout"
+                    label="Check-out"
+                    value={checkout}
+                    minDate={checkin !== null ? checkin : ''}
+                    onChange={(newValue) => {
+                      setCheckout(newValue);
+                    }}
+                    renderInput={(params) => (
+                      <TextField
+                        // fullWidth
+                        {...params}
+                      // {...register("birthday")}
+                      // error={!!errors.birthday}
+                      // helperText={errors.birthday ? errors.birthday.message : ""}
+                      />
+                    )}
+                  />
+                </Grid>
+              </LocalizationProvider>
+              {/* <Grid item xs={2}>
               <FormControl sx={{ m: 1, minWidth: 100 }} size="small" >
                 <TextField
                   label="Size"
@@ -135,44 +170,70 @@ function BodyUser() {
                   size="small" />
               </FormControl>
             </Grid> */}
+              <Grid item xs={1} sx={{ marginLeft: '10px', padding: '2px 0 0 20px' }} >
+                <Box sx={{
+                  backgroundColor: '#673ab7',
+                  width: '48px',
+                  borderRadius: '10px'
 
-          </Grid>
+                }}>
+
+                  <IconButton onClick={(e) => { console.log("search") }}>
+                    <SearchOutlinedIcon fontSize="large" sx={{ color: '#fff' }} />
+                  </IconButton>
+                </Box>
+              </Grid>
+
+            </Grid>
+          </Box>
+
+
         </Box>
-
-
-      </Box>
-      <Box component='section'
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          // flexDirection: 'column',
-          // height: '200px',
-        }}
-      >
-        <Box component='div'
+        <Box component='section'
           sx={{
-            width: '70vw',
-            marginTop: '50px',
-            // display: 'flex',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            // flexDirection: 'column',
+            // height: '200px',
           }}
         >
-          <Grid container spacing={4}>
-            <Grid item xs={4}>
-              <CardUser img={room.room1} />
-            </Grid>
-            <Grid item xs={4}>
-              <CardUser img={room.room2} />
-            </Grid>
-            <Grid item xs={4}>
-              <CardUser img={room.room3} />
+          <Box component='div'
+            sx={{
+              width: '80vw',
+              // marginTop: '10px',
+              // display: 'flex',
+            }}
+          >
+            <Grid container spacing={4}>
+              <Grid item xs={4} sx={{ marginTop: '20px' }}>
+                <CardUser img={room.room1} />
+              </Grid>
+              <Grid item xs={4} sx={{ marginTop: '20px' }}>
+                <CardUser img={room.room2} />
+              </Grid>
+              <Grid item xs={4} sx={{ marginTop: '20px' }}>
+                <CardUser img={room.room3} />
+              </Grid>
+              <Grid item xs={4} sx={{ marginTop: '20px' }}>
+                <CardUser img={room.room3} />
+              </Grid>
+
             </Grid>
 
-          </Grid>
-
+          </Box>
         </Box>
+      </main>
+
+      <Box component='footer'
+        sx={{
+          height: "80px"
+        }}
+      >
+
       </Box>
-    </main>
+    </>
+
   )
 }
 
--- src/components/client/container/CardUser.js
+++ src/components/client/container/CardUser.js
@@ -1,38 +1,62 @@
 import React from 'react'
-import { Box, Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material'
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, Box, ThemeProvider } from '@mui/material'
+import { Link } from 'react-router-dom'
+// import {  } from '@mui/material'
+import theme from '../../../theme'
 
+import SignalWifi2BarIcon from '@mui/icons-material/SignalWifi2Bar';
+import BathtubIcon from '@mui/icons-material/Bathtub';
 
 function CardUser({ img }) {
-  // console.log(img)
+
+
   return (
-    // <Box component='div'
-    //   sx={{
-    //     width: '70vw',
-    //     marginTop: '100px'
-    //   }}
-    // >
-    // </Box>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image={img}
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <>
+      <ThemeProvider theme={theme}>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={img}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Junior
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+            </Typography>
+            <Box component='div'
+              sx={{
+                display: 'flex',
+                // justifyContent: 'space-between'
+                marginTop: '5px'
+              }}
+            >
+              {/* <Box component='div'
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center'
+                }}
+              >
+              </Box> */}
+              <SignalWifi2BarIcon fontSize="small" sx={{ color: '#767A83', margin: '-1px 4px 0 0' }} />
+              <Typography>Wifi</Typography>
+
+              <BathtubIcon fontSize="small" sx={{ color: '#767A83', margin: '-1px 4px 0 6px' }} />
+              <Typography>2</Typography>
+            </Box>
+          </CardContent>
+          <CardActions>
+            <Button size="small" component={Link} to='/room'>Learn More</Button>
+          </CardActions>
+        </Card>
+      </ThemeProvider>
+    </>
+
 
   )
 }
